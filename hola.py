5a30eca4192248d13815bb8bbefe07b815b41660
Gema Anton <gema.anlub@gmail.com>
2025-07-23 10:39:27 +0200

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,13 @@
+print("Hola Mundo")
+print()  # Adds a blank line
+
+print("El resultado de 8 * 8 es:", 8 * 8)
+print("\n")  # Adds two blank lines
+
+Gema = "Gema"
+print("El nombre es:", Gema)
+print("\n" * 3)  #  Adds three blank lines at the end
+
+name = "Gema"
+print(name[2])  # Prints the type of the variable 'name'
+
